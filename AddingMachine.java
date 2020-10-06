e3ab2a3efe62b304cb8afd326b32386e252751a9
CarsonDarling1 <72428910+CarsonDarling1@users.noreply.github.com>
2020-10-05 22:07:23 -0700

    Create AddingMachine.java

---
+++ AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () {
+        return 0;
+    }   //Returns the total of the adding machine
+
+    public void add (int value) {           //Simple adding function, doesn't return
+    }
+
+    public void subtract (int value) {      //Simple adding function, returns nothing
+    }
+
+    public String toString () {             //Prints all operations
+        return "";
+    }
+
+    public void clear() {                   //Clears the current adding machine
+    }
+}
